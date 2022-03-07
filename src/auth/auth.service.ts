04a786c5785b750de58fc1d0fcac2adeb70bd7c1
Mohammad Twin <scientists.twin@gmail.com>
2022-03-07 13:08:18 +0400

    feature: add some mock user data

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,4 +5,29 @@ export class AuthService {
   loginUser(userName, passowrd) {
     return 'ok';
   }
+
+  mockUserInformation() {
+    return [
+      {
+        userName: 'ilia',
+        password: '123',
+        balance: 500,
+      },
+      {
+        userName: 'nuno',
+        password: '123',
+        balance: 200,
+      },
+      {
+        userName: 'alex',
+        password: '123',
+        balance: 50,
+      },
+      {
+        userName: 'tina',
+        password: '123',
+        balance: 100,
+      },
+    ];
+  }
 }
